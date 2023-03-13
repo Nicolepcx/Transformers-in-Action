afce03750f4d55df89c043a058c21256c4385287
Königstein Nicole <nicole.koenigstein@students.fhnw.ch>
2023-03-13 07:28:17 +0100

    corrected ch03 requirements and corrected typo in setup

--- utils/setup.py
+++ utils/setup.py
@@ -9,6 +9,6 @@ def useGPU():
     if not is_gpu_available:
         print(f"No GPU detected! This notebook will be\033[1;35m\033[5m very slow {snail}. \033[0m")
         print(f"Consider uploading the notebook to one of the cloud platforms \033[1;31m\033[5m for much faster computation\033[0m {rocket}.\n")
-        print(f"{check} \u001b[32mAlso, remember you might have change the runtime type of your notebook!\032")
+        print(f"{check} \u001b[32mAlso, remember you might have to change the runtime type of your notebook!\032")
     else:
         print(f"Have fun with this chapter!{party_emoji}")
