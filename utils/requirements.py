5f4fc8d459ca7bac4beb68bf67afe8e0ea11b825
Königstein Nicole <nicole.koenigstein@students.fhnw.ch>
2023-03-13 07:28:32 +0100

    corrected ch03 requirements and corrected typo in setup

--- utils/requirements.py
+++ utils/requirements.py
@@ -1,43 +1,33 @@
 import subprocess
 
-def install(
-    chapter_03=False,
-):
-    """
-    Installs the required packages for a specific chapter of the Transformers-in-Action book.
-    Args:
-    chapter_03 (bool): Flag to install packages required for Chapter 3. Default is False.
 
-    Returns:
-    None
-
-    Raises:
-    None
-    """
+def install_base_packages():
+    packages = ["transformers==4.26.1", "datasets==2.10.1"]
     check = u'\u2705'
-
-    print("Installing requirements...\n")
-    base_cmd = "python -m pip install transformers==4.26.1 datasets==2.10.1".split()
-    process_scatter = subprocess.run(
-        base_cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    eval_cmd = "python -m pip install summa==1.2.0 evaluate==0.4.0".split()
-    process_scatter = subprocess.run(
-        eval_cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    if chapter_03:
-        print("Installing requirements for chapter 3...")
-        chapter_03_cmd = "python -m pip install rouge_score==1.4.0".split()
+    print("\033[1mInstalling base requirements...\n\033[0m")
+    for package in packages:
         process_scatter = subprocess.run(
-            chapter_03_cmd,
+            ["pip", "install", package],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        print("Chapter 3 installation completed!\n")
+        if process_scatter.returncode != 0:
+            print(f"Installation of {package} failed with error:\n{process_scatter.stderr.decode('utf-8')}")
+        else:
+            print(f"{check} {package} installation completed successfully!\n")
 
-    print(f"{check} All installations completed!")
+
+def install_required_packages_ch03():
+    packages = ["summa==1.2.0", "evaluate==0.4.0", "rouge_score==0.1.2"]
+    check = u'\u2705'
+    print("\033[1mInstalling chapter 3 requirements...\n\033[0m")
+    for package in packages:
+        process_scatter = subprocess.run(
+            ["pip", "install", package],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if process_scatter.returncode != 0:
+            print(f"Installation of {package} failed with error:\n{process_scatter.stderr.decode('utf-8')}")
+        else:
+            print(f"{check} {package} installation completed successfully!\n")
