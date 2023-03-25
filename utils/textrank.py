830b720ac96c9e43c8c320fe8e7d4eb817407fbf
Königstein Nicole <nicole.koenigstein@students.fhnw.ch>
2023-03-25 04:57:58 +0100

    updated TextRank class to print out sentences with weights and to reflect more the provided equation

--- utils/textrank.py
+++ utils/textrank.py
@@ -1,37 +1,50 @@
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 nltk.download('punkt')
 
 
 class TextRank:
     """
-    A class for creating a TextRank graph from a list of sentences.
+        A class for creating a TextRank graph from a text and computing sentence ranks.
 
-    Attributes:
-    -----------
-    sentences : list
-        A list of strings representing the sentences.
+        Attributes:
+        -----------
+        sent_detector : nltk.tokenize.punkt.PunktSentenceTokenizer
+            A sentence tokenizer for splitting text into sentences.
 
-    graph : networkx.classes.graph.Graph
-        A graph of the sentences, where nodes represent the sentences and edges
-        represent their similarity.
+        c : sklearn.feature_extraction.text.CountVectorizer
+            A count vectorizer for creating a bag-of-words representation of sentences.
 
-    Methods:
-    --------
-    create_graph():
-        Create a graph from the list of sentences using bag of words, TF-IDF
-        transformation and cosine similarity.
+        tfidf_transformer : sklearn.feature_extraction.text.TfidfTransformer
+            A TF-IDF transformer for converting a bag-of-words matrix into a TF-IDF matrix.
 
-    plot_graph():
-        Plot the TextRank graph using a circular layout and display it.
+        Methods:
+        --------
+        split_sentences(text: str) -> list:
+            Split the input text into sentences.
 
-    """
+        bag_of_words(sentences: list) -> scipy.sparse.csr_matrix:
+            Create a bag-of-words representation for the input sentences.
 
+        pagerank(G: nx.Graph, delta: float, max_iter: int, tol: float) -> np.ndarray:
+            Compute the PageRank of the nodes in the graph G.
+
+        rank(G: nx.Graph, delta: float, max_iter: int, tol: float) -> np.ndarray:
+            Compute the normalized ranks of the nodes in the graph G.
+
+        create_graph(sentences: list, delta: float) -> nx.Graph:
+            Create a graph for the input sentences using the TF-IDF similarity.
+
+        plot_graph(G: nx.Graph):
+            Plot the graph G with edge weights.
+
+        print_sentence_weights(G: nx.Graph, sentences: list):
+            Print the sentence weights in the graph G.
+        """
     def __init__(self):
         self.sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
         self.c = CountVectorizer()
@@ -52,12 +65,12 @@ class TextRank:
 
     def bag_of_words(self, sentences):
         """
-        Convert a list of sentences into a sparse matrix representation of a Bag of Words.
+        Convert a  block of text into a sparse matrix representation of a Bag of Words.
 
         Parameters:
         -----------
-        sentences: list
-            A list of strings representing the sentences.
+        text (str):
+            A block of strings representing the sentences.
 
         Returns:
         --------
@@ -70,8 +83,91 @@ class TextRank:
         bag_words = self.c.fit_transform(sentences)
         return bag_words
 
-    def create_graph(self, sentences):
-        '''
+    def pagerank(self, G, delta=0.85, max_iter=100, tol=1e-6):
+        """
+        Compute the PageRank of the nodes in the graph G.
+
+        The PageRank algorithm is an iterative method that estimates the importance
+        of nodes in a graph based on the structure of the graph and the connections
+        between nodes. This implementation uses the power iteration method to compute
+        PageRank values.
+
+        Parameters:
+        -----------
+        G : networkx.classes.graph.Graph
+            The input graph.
+        delta : float, optional
+            The damping factor for the PageRank algorithm (default is 0.85).
+        max_iter : int, optional
+            The maximum number of iterations for the PageRank algorithm (default is 100).
+        tol : float, optional
+            The tolerance for convergence of the PageRank algorithm (default is 1e-6).
+
+        Returns:
+        --------
+        ranks : numpy.ndarray
+            A 1D array containing the PageRank values for each node in the graph. The
+            values are proportional to the importance of the nodes in the graph.
+        """
+
+        N = len(G)
+        ranks = np.ones(N) / N
+        adjacency_matrix = nx.to_numpy_array(G)
+
+        # Calculate the out-degree for each node
+        out_degrees = np.sum(adjacency_matrix, axis=1)
+
+        for _ in range(max_iter):
+            # This line corresponds to the (1 - delta) / N term in both equations
+            new_ranks = np.ones(N) * (1 - delta) / N
+            for i in range(N):
+                for j in range(N):
+                    # This condition checks if there's an edge between nodes j and i
+                    if adjacency_matrix[j, i] > 0:
+                        # This line corresponds to the summation in both equations,
+                        # with omega_ji being the adjacency_matrix[j, i] term,
+                        # ranks[j] corresponding to r(V_j^(k)), and out_degrees[j]
+                        # We representing the denominator in the fraction (c_j or the sum of the edge weights)
+                        new_ranks[i] += delta * ranks[j] * adjacency_matrix[j, i] / out_degrees[j]
+            # This line checks for convergence
+            if np.linalg.norm(new_ranks - ranks) < tol:
+                break
+            ranks = new_ranks
+
+        return ranks
+
+    def rank(self, G, delta=0.85, max_iter=100, tol=1e-6):
+        """
+        Compute the normalized PageRank of the nodes in the graph G.
+
+        This method computes the PageRank values for the nodes in the graph and
+        normalizes them so that their sum equals 1. This can be useful for comparing
+        the relative importance of nodes in the graph.
+
+        Parameters:
+        -----------
+        G : networkx.classes.graph.Graph
+           The input graph.
+        delta : float, optional
+           The damping factor for the PageRank algorithm (default is 0.85).
+        max_iter : int, optional
+           The maximum number of iterations for the PageRank algorithm (default is 100).
+        tol : float, optional
+           The tolerance for convergence of the PageRank algorithm (default is 1e-6).
+
+        Returns:
+        --------
+        normalized_ranks : numpy.ndarray
+           A 1D array containing the normalized PageRank values for each node in the graph.
+           The values sum to 1 and are proportional to the relative importance of the nodes
+           in the graph.
+        """
+
+        ranks = self.pagerank(G, delta, max_iter, tol)
+        return ranks / ranks.sum()
+
+    def create_graph(self, sentences, delta=0.85):
+        """
         create_graph(sentences)
 
         This function creates a graph from a list of sentences using bag of words,
@@ -84,7 +180,7 @@ class TextRank:
         - G (networkx.classes.graph.Graph): A graph of the sentences, where nodes
               represent the sentences and edges represent their similarity.
 
-        '''
+        """
         # Convert the sentences to a bag of words matrix
         bag_of_words = self.bag_of_words(sentences)
 
@@ -92,10 +188,12 @@ class TextRank:
         tfidf_matrix = self.tfidf_transformer.fit_transform(bag_of_words)
 
         # Compute the cosine similarity matrix
-        cosine_similarity = np.dot(tfidf_matrix, tfidf_matrix.T)
-
+        adjacency_matrix = np.dot(tfidf_matrix, tfidf_matrix.T)
         # Create a graph from the similarity matrix
-        G = nx.from_scipy_sparse_array(cosine_similarity)
+        G = nx.from_numpy_array(adjacency_matrix)
+
+        for u, v, weight in G.edges(data=True):
+            weight['weight'] = adjacency_matrix[u, v]
 
         return G
 
@@ -112,7 +210,6 @@ class TextRank:
         --------
         None
         """
-
         # Remove self-loops from the graph
         G.remove_edges_from(nx.selfloop_edges(G))
 
@@ -140,3 +237,39 @@ class TextRank:
 
         # Display the plot
         plt.show()
+
+    def print_sentence_weights(self, G, sentences):
+        """
+        Print the sentence weights in the TextRank graph.
+
+        This method iterates through the edges of the graph G and prints the
+        corresponding sentences and their similarity weight. It can be useful for
+        visualizing and understanding the relationships between sentences in the graph.
+
+        Parameters:
+        -----------
+        G : networkx.classes.graph.Graph
+            The input graph representing sentence relationships.
+        sentences : list of str
+            A list of sentences corresponding to the nodes in the graph G.
+
+        Returns:
+        --------
+        None
+        """
+
+        # Print the header
+        print("Sentence Weights:")
+
+        # Iterate through the edges of the graph
+        for u, v, weight in G.edges(data=True):
+            # Get the weight value from the edge data
+            weight_value = weight['weight']
+
+            # Print the sentences and their corresponding weight
+            print(f"Sentence {u}: {sentences[u]}")
+            print(f"Sentence {v}: {sentences[v]}")
+            print(f"Weight: {weight_value:.4f}\n")
+
+
+
