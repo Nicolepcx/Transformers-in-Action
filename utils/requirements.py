c1b51f69799af8b58967e69a808dd130c32e8075
Königstein Nicole <nicole.koenigstein@students.fhnw.ch>
2023-04-12 05:22:57 +0200

    added requirements for chapter 4

--- utils/requirements.py
+++ utils/requirements.py
@@ -31,3 +31,19 @@ def install_required_packages_ch03():
             print(f"Installation of {package} failed with error:\n{process_scatter.stderr.decode('utf-8')}")
         else:
             print(f"{check} {package} installation completed successfully!\n")
+
+def install_required_packages_ch04():
+    packages = ["transformers == 4.26.1", "datasets == 2.10.1", "evaluate==0.4.0", "sacrebleu", "pyarrow==9.0.0", "sentencepiece"]
+
+    check = u'\u2705'
+    print("\033[1mInstalling chapter 4 requirements...\n\033[0m")
+    for package in packages:
+        process_scatter = subprocess.run(
+            ["pip", "install", package],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if process_scatter.returncode != 0:
+            print(f"Installation of {package} failed with error:\n{process_scatter.stderr.decode('utf-8')}")
+        else:
+            print(f"{check} {package} installation completed successfully!\n")
